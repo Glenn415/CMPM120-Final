52b3bbb8e16075050e54e7300943429db90a4f4d
Glenn415 <35755081+Glenn415@users.noreply.github.com>
2018-05-07 20:57:37 -0700

    commit: game states

---
+++ myGame/javascript/script.js
@@ -0,0 +1,104 @@
+var game = new Phaser.Game(800, 600, Phaser.AUTO);
+
+// Main State ==================================================
+var Menu = function(game){};
+Menu.prototype = {
+	// preload assets ================================
+	preload: function(){
+		console.log("Menu: preload");
+		game.load.atlas("button", "assets/img/buttons.png", "assets/img/buttons.json");
+	},
+
+	// place assets ==================================
+	create: function(){
+		console.log("Menu: create");
+	},
+
+	// update, run game loop =========================
+	update: function(){
+		// load 'Tutorial' state when user pressed ENTER key
+		if(game.input.keyboard.isDown(Phaser.Keyboard.ENTER)) {
+			game.state.start('Tutorial');
+		}
+	}
+}
+
+// Tutorial State ==============================================
+var Tutorial = function(game){};
+Tutorial.prototype = {
+	// preload assets ================================
+	preload: function(){
+		console.log("Tutorial: preload");
+	},
+
+	// place assets ==================================
+	create: function(){
+		console.log("Tutorial: create");
+	},
+
+	// update, run the game loop =====================
+	update: function(){
+		// load 'GamePlay' state when user pressed ENTER key
+		if(game.input.keyboard.isDown(Phaser.Keyboard.ENTER)) {
+			game.state.start('GamePlay');
+		}
+	}
+}
+
+// Gameplay state ==============================================
+var GamePlay = function(game){};
+GamePlay.prototype = {
+	// preload assets ================================
+	preload: function(){
+		console.log("GamePlay: preload");
+	},
+
+	// place assets ==================================
+	create: function(){
+		console.log("GamePlay: create");
+	},
+
+	// update, run the game loop =====================
+	update: function(){
+		// load 'GamePlay' state when user pressed ENTER key
+		if(game.input.keyboard.isDown(Phaser.Keyboard.ENTER)) {
+			game.state.start('GameOver');
+		}
+	},
+
+	// debuging method ===============================
+	render: function(){}
+}
+
+// GameOver State ==============================================
+var GameOver = function(game){};
+GameOver.prototype = {
+	// preload assets ================================
+	preload: function(){
+		console.log("GameOver: preload");
+	},
+
+	// place assets ==================================
+	create: function(){
+		console.log("GameOver: create");
+	},
+
+	// update, run the game loop =====================
+	update: function(){
+		// load 'GamePlay' state when user pressed ENTER key
+		if(game.input.keyboard.isDown(Phaser.Keyboard.ENTER)) {
+			game.state.start('Menu');
+		}
+	}
+}
+
+// Add the states to the StateManager and start in Menu
+game.state.add("Menu", Menu);
+game.state.add("Tutorial", Tutorial);
+game.state.add("GamePlay", GamePlay);
+game.state.add("GameOver", GameOver);
+game.state.start("Menu");
+
+// Helper functions ============================================
+
+
