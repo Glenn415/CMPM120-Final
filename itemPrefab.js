c3e97c510a947871b1bce8d710f3f3266dddcb93
Zuola <zguoerlu@ucsc.edu>
2018-05-09 17:16:43 -0700

    added enableDrag()

--- itemPrefab.js
+++ itemPrefab.js
@@ -10,15 +10,8 @@ function Item(game, x, y, key, frame){
 	game.physics.enable(this);
 	this.body.collideWorldBounds = true;
 	this.inputEnabled = true;
+	this.input.enableDrag(); //enable click and drag
 }
 
 Item.prototype = Object.create(Phaser.Sprite.prototype);
 Item.prototype.constructor = Item;
-//overRide update function to check for mouse events
-Item.prototype.update = function(){
-	//click and drag
-	if ( this.body.x == game.input.mousePointer.x && this.body.y == game.input.mousePointer.y && game.input.activePointer.isDown){
-		this.body.x = game.input.mousePointer.x;
-		this.body.y = game.input.mousePointer.y;
-	}
-}
--- knife.js
+++ knife.js
@@ -16,10 +16,6 @@ Knife.prototype = Object.create(Item);
 Knife.prototype.constructor = Knife;
 //overRide update function to check for mouse events
 Knife.prototype.update = function(){
-	//click and drag
-	if ( this.body.x == game.input.mousePointer.x && this.body.y == game.input.mousePointer.y && game.input.activePointer.isDown){
-		this.body.x = game.input.mousePointer.x;
-		this.body.y = game.input.mousePointer.y;
-	}
+	
 	//if dropped onto NPC.sprite, kill itself and the NPC
 }
