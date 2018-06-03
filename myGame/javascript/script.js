3f4197f363a898495f7be60122f5572339758834
Glenn415 <35755081+Glenn415@users.noreply.github.com>
2018-06-03 16:08:17 -0700

    Cutscene buttons

--- myGame/javascript/script.js
+++ myGame/javascript/script.js
@@ -402,6 +402,7 @@ CutSceneAccept.prototype = {
 	create: function(){
 		console.log("CutSceneAccept: create");
 		game.stage.backgroundColor = "#707070";
+		game.add.button(590, 520, "btnNext", gotoGame, this);
 	},
 
 	//checks to see which accept cutscene to display depending on the the current questNumber
@@ -476,6 +477,7 @@ CutSceneDecline.prototype = {
 	create: function(){
 		console.log("CutSceneDecline: create");
 		game.stage.backgroundColor = "#707070";
+		game.add.button(590, 520, "btnNext", gotoGame, this);
 		//text = game.add.text(0, 75, "Decline cut Scene");
 		//text.anchor.set(0.5);
 	},
@@ -552,6 +554,7 @@ CutSceneKill.prototype = {
 	create: function(){
 		console.log("CutSceneKill: create");
 		game.stage.backgroundColor = "#707070";
+		game.add.button(590, 520, "btnNext", gotoGame, this);
 	},
 
 	//checks to see which kill cutscene to display depending on the the current questNumber
